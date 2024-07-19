2f204cb1c813e448ffd43bae39be2c2625abfc66
Marcin <wanago.marcin@gmail.com>
2024-07-19 02:19:10 +0200

    feat: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,9 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
