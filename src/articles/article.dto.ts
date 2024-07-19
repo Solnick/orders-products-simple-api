412b68ca5f187f656d357600830ff72ca4095155
Marcin <wanago.marcin@gmail.com>
2024-07-19 02:13:33 +0200

    docs: set up Swagger

--- src/articles/article.dto.ts
+++ src/articles/article.dto.ts
@@ -1,11 +1,14 @@
 import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ArticleDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   content: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   title: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Simple API for managing articles')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
